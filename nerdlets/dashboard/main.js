41673d8aaa52fbb28526ceee338250c433b3bc1c
Sebastián Medina <smedina@rmpservices.com.co>
2019-11-16 00:25:12 -0500

    C-SuiteDashboard

--- nerdlets/dashboard/main.js
+++ nerdlets/dashboard/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid, GridItem, Stack, StackItem, FunnelChart, ChartGroup, AreaChart, BarChart, LineChart, TableChart, PieChart, Button, TextField, Modal, Toast } from 'nr1';
+import { Grid, GridItem, Stack, StackItem, FunnelChart, StackedBarChart, BillboardChart, ChartGroup, AreaChart, BarChart, LineChart, TableChart, PieChart, Button, TextField, Modal, Toast } from 'nr1';
 
 export default class MyNerdlet extends React.Component {
     static propTypes = {
@@ -61,9 +61,12 @@ export default class MyNerdlet extends React.Component {
         const since = ` SINCE ${duration / 1000 / 60} MINUTES AGO `;
         const errors = `SELECT count(error) FROM Transaction TIMESERIES`;
         const throughput = `SELECT count(*) as 'throughput' FROM Transaction TIMESERIES`;
-        const transaction_apdex_by_appname = `SELECT count(*) as 'transaction', apdex(duration) as 'apdex' FROM Transaction limit 25`;
-        const plans_funnel = `SELECT funnel(session, where pageUrl='http://webportal.telco.nrdemo-sandbox.com/index.html' as 'Home', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/browse/plans' as 'Plan Catalog', where pageUrl like 'http://webportal.telco.nrdemo-sandbox.com/browse/plans/%' as 'Plan Details', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/login.jsp'  as 'Login', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/shoppingcart' as 'Shopping Cart', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/checkout' as 'Checkout') from PageView`
-        const phones_funnel = `SELECT funnel(session, where pageUrl='http://webportal.telco.nrdemo-sandbox.com/index.html' as 'Home', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/browse/phones' as 'Phone Catalog', where pageUrl like 'http://webportal.telco.nrdemo-sandbox.com/browse/phones/%' as 'Phone Overview', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/login.jsp'  as 'Login',  where pageUrl='http://webportal.telco.nrdemo-sandbox.com/shoppingcart' as 'Shopping Cart', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/checkout' as 'Checkout') from PageView`
+        const transaction_apdex_by_appname = `SELECT count(*) as 'transaction', apdex(duration) as 'apdex' FROM Transaction limit 25 FACET appName`;
+        const plans_funnel = `SELECT funnel(session, where pageUrl='http://webportal.telco.nrdemo-sandbox.com/index.html' as 'Home', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/browse/plans' as 'Plan Catalog', where pageUrl like 'http://webportal.telco.nrdemo-sandbox.com/browse/plans/%' as 'Plan Details', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/login.jsp'  as 'Login', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/shoppingcart' as 'Shopping Cart', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/checkout' as 'Checkout') from PageView`;
+        const phones_funnel = `SELECT funnel(session, where pageUrl='http://webportal.telco.nrdemo-sandbox.com/index.html' as 'Home', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/browse/phones' as 'Phone Catalog', where pageUrl like 'http://webportal.telco.nrdemo-sandbox.com/browse/phones/%' as 'Phone Overview', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/login.jsp'  as 'Login',  where pageUrl='http://webportal.telco.nrdemo-sandbox.com/shoppingcart' as 'Shopping Cart', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/checkout' as 'Checkout') from PageView`;
+        const customers_by_country = `SELECT count(*) FROM PageView` + since + `FACET countryCode`;
+        const live_sessions = `SELECT Count(session) from PageView`;
+        const browser_usage = "SELECT SUM(browserPageViewCount) as Pageviews FROM NrDailyUsage WHERE `productLine` = 'Browser' AND `usageType` = 'Application' AND `isPrimaryApp` = 'true' FACET monthOf(timestamp) SINCE 12 months ago LIMIT 100"
         const chartHeight = 250;
 
         return <React.Fragment>
@@ -78,42 +81,6 @@ export default class MyNerdlet extends React.Component {
             <Grid>
                 <GridItem columnStart={1} columnEnd={12}>
                     <h1><b>CEO Dashboard</b></h1>
-                    <form onSubmit={this.onSubmit}>
-                        <Stack fullWidth>
-                            <StackItem grow={true}>
-                                <TextField multiline spacingType={[TextField.SPACING_TYPE.LARGE]}
-                                    value={this.state.value}
-                                    onChange={this.handleChange}
-                                />
-                            </StackItem>
-                            <StackItem>
-                                <Button type={Button.TYPE.PRIMARY}>Facet</Button>
-                            </StackItem>
-                        </Stack>
-                        <Modal
-                            hidden={this.state.hideModal}
-                            onClose={() => { this.setState({ facet: '', value: '', hideModal: true }) }}
-                        >
-                            <Stack>
-                                <StackItem>
-                                    <h1 className="Modal-headline">Are you sure you want to apply this facet?</h1>
-                                    <p className="facet-value">Facet by: <strong>{this.state.value}</strong></p>
-                                    <Stack>
-                                        <StackItem>
-                                            <Button
-                                                onClick={this.rejectFacet}
-                                            >No</Button>
-                                        </StackItem>
-                                        <StackItem>
-                                            <Button
-                                                onClick={this.confirmFacet}
-                                            >Yes</Button>
-                                        </StackItem>
-                                    </Stack>
-                                </StackItem>
-                            </Stack>
-                        </Modal>
-                    </form>
                 </GridItem>
                 <GridItem columnStart={1} columnEnd={6}>
                     <h4>Transactions by Application</h4>
@@ -125,6 +92,15 @@ export default class MyNerdlet extends React.Component {
                     />
                 </GridItem>
                 <GridItem columnStart={7} columnEnd={12}>
+                    <h4>Customers by Country</h4>
+                    <PieChart
+                        style={{ height: `${chartHeight}px` }}
+                        className="chart"
+                        query={customers_by_country}
+                        accountId={this.accountId}
+                    />
+                </GridItem>
+                <GridItem columnStart={1} columnEnd={6}>
                     <h4>Customer Journey <b>(Plans)</b></h4>
                     <FunnelChart
                         style={{ height: `${chartHeight}px` }}
@@ -140,8 +116,26 @@ export default class MyNerdlet extends React.Component {
                         className="chart"
                         accountId={this.accountId}
                         query={phones_funnel + since} />
+                </GridItem>
+                <GridItem columnStart={7} columnEnd={12}>
+                    <h4>Live Application Sessions</h4>
+                    <BillboardChart
+                    style={{ height: `${chartHeight}px` }}
+                        className="chart"
+                        accountId={this.accountId}
+                        query={live_sessions + since} />
 
                 </GridItem>
+                <GridItem columnStart={1} columnEnd={6}>
+                    <h4>Browser Yearly Usage</h4>
+                    <StackedBarChart
+                    style={{ height: `${chartHeight}px` }}
+                        className="chart"
+                        accountId={this.accountId}
+                        query={browser_usage} />
+
+                </GridItem>
+
             </Grid>
         </React.Fragment >
     }
