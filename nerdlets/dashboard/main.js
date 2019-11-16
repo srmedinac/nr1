20c68c1b77946a09a89c855473b3fa5fdf6a6c1d
Jose Daniel Suárez <jdsuarezj@unal.edu.co>
2019-11-15 23:18:27 -0500

    Fix frontend CEO Dashboard

--- nerdlets/dashboard/main.js
+++ nerdlets/dashboard/main.js
@@ -64,6 +64,7 @@ export default class MyNerdlet extends React.Component {
         const transaction_apdex_by_appname = `SELECT count(*) as 'transaction', apdex(duration) as 'apdex' FROM Transaction limit 25`;
         const plans_funnel = `SELECT funnel(session, where pageUrl='http://webportal.telco.nrdemo-sandbox.com/index.html' as 'Home', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/browse/plans' as 'Plan Catalog', where pageUrl like 'http://webportal.telco.nrdemo-sandbox.com/browse/plans/%' as 'Plan Details', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/login.jsp'  as 'Login', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/shoppingcart' as 'Shopping Cart', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/checkout' as 'Checkout') from PageView`
         const phones_funnel = `SELECT funnel(session, where pageUrl='http://webportal.telco.nrdemo-sandbox.com/index.html' as 'Home', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/browse/phones' as 'Phone Catalog', where pageUrl like 'http://webportal.telco.nrdemo-sandbox.com/browse/phones/%' as 'Phone Overview', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/login.jsp'  as 'Login',  where pageUrl='http://webportal.telco.nrdemo-sandbox.com/shoppingcart' as 'Shopping Cart', where pageUrl='http://webportal.telco.nrdemo-sandbox.com/checkout' as 'Checkout') from PageView`
+        const chartHeight = 250;
 
         return <React.Fragment>
             {this.state.showToast &&
@@ -74,100 +75,74 @@ export default class MyNerdlet extends React.Component {
                     onHideEnd={() => { this.setState({ showToast: false }) }}
                 />
             }
-            <ChartGroup>
-                <Grid className="grid">
-                    <GridItem
-                        columnSpan={12}>
-                        <form onSubmit={this.onSubmit}>
-                            <Stack fullWidth>
-                                
-                                <StackItem grow={true}>
-                                    <TextField multiline spacingType={[TextField.SPACING_TYPE.LARGE]}
-                                        value={this.state.value}
-                                        onChange={this.handleChange}
-                                    />
-                                </StackItem>
-                                <StackItem>
-                                    <Button type={Button.TYPE.PRIMARY}>Facet</Button>
-                                </StackItem>
-                            </Stack>
-                            <Modal
-                                hidden={this.state.hideModal}
-                                onClose={() => { this.setState({ facet: '', value: '', hideModal: true }) }}
-                            >
-                                <Stack>
-                                    <StackItem>
-                                        <h1 className="Modal-headline">Are you sure you want to apply this facet?</h1>
-                                        <p className="facet-value">Facet by: <strong>{this.state.value}</strong></p>
-                                        <Stack>
-                                            <StackItem>
-                                                <Button
-                                                    onClick={this.rejectFacet}
-                                                >No</Button>
-                                            </StackItem>
-                                            <StackItem>
-                                                <Button
-                                                    onClick={this.confirmFacet}
-                                                >Yes</Button>
-                                            </StackItem>
-                                        </Stack>
-                                    </StackItem>
-                                </Stack>
-                            </Modal>
-                        </form>
-                    </GridItem>
-                    <GridItem
-                        columnSpan={6}>
-                            
-                        <Stack
-                            fullWidth
-                            className="side-col"
-                            gapType={Stack.GAP_TYPE.LOOSE}
-                            directionType={Stack.DIRECTION_TYPE.VERTICAL}>
-                            <StackItem>
-                            <h4>Transactions by Application</h4>
-                                <PieChart
-                                    className="chart"
-                                    query={transaction_apdex_by_appname + since + this.state.facet}
-                                    accountId={this.accountId}
+            <Grid>
+                <GridItem columnStart={1} columnEnd={12}>
+                    <h1><b>CEO Dashboard</b></h1>
+                    <form onSubmit={this.onSubmit}>
+                        <Stack fullWidth>
+                            <StackItem grow={true}>
+                                <TextField multiline spacingType={[TextField.SPACING_TYPE.LARGE]}
+                                    value={this.state.value}
+                                    onChange={this.handleChange}
                                 />
                             </StackItem>
                             <StackItem>
-                            <h4>Customer Journey (Plans)</h4>
-                                <FunnelChart accountId={this.accountId} query={plans_funnel + since} />
+                                <Button type={Button.TYPE.PRIMARY}>Facet</Button>
                             </StackItem>
                         </Stack>
-                    </GridItem>
-                    <GridItem columnSpan={6}>
-                        <Stack
-                            fullWidth
-                            className="side-col"
-                            gapType={Stack.GAP_TYPE.LOOSE}
-                            directionType={Stack.DIRECTION_TYPE.HORIZONTAL}>
-                            <StackItem>
-                            <h4>Customer Journey (Phones)</h4>
-                                <FunnelChart accountId={this.accountId} query={phones_funnel + since} />
-                            </StackItem>
+                        <Modal
+                            hidden={this.state.hideModal}
+                            onClose={() => { this.setState({ facet: '', value: '', hideModal: true }) }}
+                        >
+                            <Stack>
+                                <StackItem>
+                                    <h1 className="Modal-headline">Are you sure you want to apply this facet?</h1>
+                                    <p className="facet-value">Facet by: <strong>{this.state.value}</strong></p>
+                                    <Stack>
+                                        <StackItem>
+                                            <Button
+                                                onClick={this.rejectFacet}
+                                            >No</Button>
+                                        </StackItem>
+                                        <StackItem>
+                                            <Button
+                                                onClick={this.confirmFacet}
+                                            >Yes</Button>
+                                        </StackItem>
+                                    </Stack>
+                                </StackItem>
                             </Stack>
-                    </GridItem>
-                </Grid>
-            </ChartGroup >
+                        </Modal>
+                    </form>
+                </GridItem>
+                <GridItem columnStart={1} columnEnd={6}>
+                    <h4>Transactions by Application</h4>
+                    <PieChart
+                        style={{ height: `${chartHeight}px` }}
+                        className="chart"
+                        query={transaction_apdex_by_appname + since + this.state.facet}
+                        accountId={this.accountId}
+                    />
+                </GridItem>
+                <GridItem columnStart={7} columnEnd={12}>
+                    <h4>Customer Journey <b>(Plans)</b></h4>
+                    <FunnelChart
+                        style={{ height: `${chartHeight}px` }}
+                        className="chart"
+                        accountId={this.accountId}
+                        query={plans_funnel + since} />
+                </GridItem>
+                <GridItem columnStart={7} columnEnd={12}>
+
+                    <h4>Customer Journey <b>(Phones)</b></h4>
+                    <FunnelChart
+                        style={{ height: `${chartHeight}px` }}
+                        className="chart"
+                        accountId={this.accountId}
+                        query={phones_funnel + since} />
+
+                </GridItem>
+            </Grid>
         </React.Fragment >
     }
 }
-//grid example
-/*<Grid>
-           {gridItems}
-           <GridItem
-               columnSpan={6}
-               style={{backgroundColor: "#FF0000"}}
-           >
-               Six Column Grid Item
-           </GridItem>
-           <GridItem
-               columnSpan={4}
-               style={{backgroundColor: "#00FF00"}}
-           >
-               Four Column Grid Item
-           </GridItem>
-       </Grid>*/
